b295ff47b30d391621a0f8df5d4a7ca83b1a5d4b
Freddy <118914591+DontFred@users.noreply.github.com>
2024-12-03 11:13:59 +0100

    Update install-2.sh

--- install-2.sh
+++ install-2.sh
@@ -9,7 +9,7 @@ loginctl enable-linger $username
 bluetoothaudioconfig='[General]
 Class = 0x20041C
 Enable = Source,Sink,Media,Socket'
-echo "$bluetoothaudioconfig" > /etc/bluetooth/audio.conf
+echo "$bluetoothaudioconfig" > /etc/bluetooth/main.conf
 
 # setup a udev rule to automatically connect the bluetooth A2DP source to the Pulseaudio sink
 cp udev/81-input-a2dp-autoconnect.rules /etc/udev/rules.d/
